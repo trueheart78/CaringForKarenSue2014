0e997ba52094594e5afabde1310afcd89035617b
Josh M <mills.joshua@gmail.com>
2014-03-21 20:36:30 -0400

    Updated the site to use better bootstrap design (columns, ya!)and a real navigation bar!

--- app.rb
+++ app.rb
@@ -3,14 +3,17 @@ require 'haml'
 
 get '/' do
   # use the views/index.haml file
+  @homeTab = true
   haml :index
 end
 
 get '/sponsors' do
+	@sponsorTab = true
 	haml :sponsors
 end
 
 get '/about' do
 	# use the views/about.haml file
+	@aboutTab = true
 	haml :about
 end
