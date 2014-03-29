d5eb76be1ba5bd59feb7d83ae5a00fff538b5ea4
Josh M <mills.joshua@gmail.com>
2014-03-29 14:41:17 -0400

    Added pages to respond to the paypal registration buttons

--- app.rb
+++ app.rb
@@ -22,4 +22,31 @@ class CaringSite < Sinatra::Base
 	  @aboutTab = true
 	  haml :about
   end
+
+  get '/registration/success' do
+    @joinTab = true
+    "Thank you for registering"
+  end
+  get '/registration/cancelled' do
+  	@joinTab = true
+  	"Your registration has been cancelled"
+  end
+
+  get '/registration/lunches/success' do
+    @joinTab = true
+    "Thank you for registering for lunch"
+  end
+  get '/registration/lunches/cancelled' do
+  	@joinTab = true
+  	"Your registration for lunch has been cancelled"
+  end
+  
+  get '/registration/sponsor/success' do
+    @joinTab = true
+    "Thank you for registering to sponsor the event"
+  end
+  get '/registration/sponsor/cancelled' do
+  	@joinTab = true
+  	"Your registration to sponsor the event has been cancelled"
+  end
 end
