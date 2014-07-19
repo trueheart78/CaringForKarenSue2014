1690b5d7822ee0636c9c23cb15784b7cb0b13b40
Josh Mills <josh@trueheart78.com>
2014-07-19 17:54:03 -0400

    Changed from HAML to ERB for the layouts

--- app.rb
+++ app.rb
@@ -1,79 +1,78 @@
 require_relative 'environment'
 require 'sinatra/base'
-require 'haml'
 
 class CaringSite < Sinatra::Base
 	before do
-	  session[:csrf] ||= Rack::Protection::Base.new(self).random_string
+		session[:csrf] ||= Rack::Protection::Base.new(self).random_string
 	end
-  get '/' do
-    # use the views/index.haml file
-    @homeTab = true
-    haml :index
-  end
+	get '/' do
+		# use the views/index.erb file
+		@homeTab = 'active'
+		erb :index
+	end
 
-  get '/join' do
-	  @joinTab = true
-	  haml :join
-  end
+	get '/join' do
+		@joinTab = 'active'
+		erb :join
+	end
 
-  post '/join' do
+	post '/join' do
 		if params[:email].empty? or params[:name].empty?
 			redirect '/join'
 		end
 		#do something with the post data, then return success
-    require 'library/emailer'
-    emailer = Emailer.new(params)
-    emailer.sendAdminEmail
-    if params[:checkout] == 'check'
-      emailer.sendUserEmail
-    end
-    "success"
+		require 'library/emailer'
+		emailer = Emailer.new(params)
+		emailer.sendAdminEmail
+		if params[:checkout] == 'check'
+			emailer.sendUserEmail
+		end
+		"success"
 	end
-  
-  get '/about' do
-	  # use the views/about.haml file
-	  @aboutTab = true
-	  haml :about
-  end
 
-  get '/donation/success' do
-    @joinTab = true
-    haml :donation_success
-  end
-  get '/donation/cancelled' do
-  	@joinTab = true
-  	haml :donation_cancelled
-  end
+	get '/about' do
+		# use the views/about.erb file
+		@aboutTab = 'active'
+		erb :about
+	end
 
-  get '/registration/success' do
-    @joinTab = true
-    haml :registration_success
-  end
-  get '/registration/cancelled' do
-  	@joinTab = true
-  	haml :registration_cancelled
-  end
+	get '/donation/success' do
+		@joinTab = 'active'
+		erb :donation_success
+	end
+	get '/donation/cancelled' do
+		@joinTab = 'active'
+		erb :donation_cancelled
+	end
 
-  get '/registration/lunches/success' do
-    @joinTab = true
-    haml :lunches_success
-  end
-  get '/registration/lunches/cancelled' do
-  	@joinTab = true
-  	haml :lunches_cancelled
-  end
+	get '/registration/success' do
+		@joinTab = 'active'
+		erb :registration_success
+	end
+	get '/registration/cancelled' do
+		@joinTab = 'active'
+		erb :registration_cancelled
+	end
 
-  get '/registration/sponsor/success' do
-    @joinTab = true
-    haml :sponsor_success
-  end
-  get '/registration/sponsor/cancelled' do
-  	@joinTab = true
-  	haml :sponsor_cancelled
-  end
-  get '/registration/check/confirmation' do
-    @joinTab = true
-    haml :check_confirmation
-  end
+	get '/registration/lunches/success' do
+		@joinTab = 'active'
+		erb :lunches_success
+	end
+	get '/registration/lunches/cancelled' do
+		@joinTab = 'active'
+		erb :lunches_cancelled
+	end
+
+	get '/registration/sponsor/success' do
+		@joinTab = 'active'
+		erb :sponsor_success
+	end
+	get '/registration/sponsor/cancelled' do
+		@joinTab = 'active'
+		erb :sponsor_cancelled
+	end
+	get '/registration/check/confirmation' do
+		@joinTab = 'active'
+		erb :check_confirmation
+	end
 end
