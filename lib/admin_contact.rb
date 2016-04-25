63729fa8969ad541f68e02c19673507a588d16bd
Josh Mills <jmills@covermymeds.com>
2016-04-25 17:01:55 -0400

    New AdminContact. Env ADMIN_NAME + ADMIN_EMAIL are now ADMIN_ADDRESS

---
+++ lib/admin_contact.rb
@@ -0,0 +1,17 @@
+class AdminContact
+  attr_reader :name, :email, :payment_type, :selected_item
+
+  ADMIN_ADDRESS = 'Dave Danielson <daveydan21@yahoo.com>'
+
+  def initialize(name, email, payment_type, selected_item)
+    @name = name
+    @email = email
+    @payment_type = payment_type
+    @selected_item = selected_item
+  end
+
+  def address
+    return ADMIN_ADDRESS unless ENV.has_key? 'ADMIN_ADDRESS' && !ENV['ADMIN_ADDRESS'].empty?
+    ENV['ADMIN_ADDRESS']
+  end
+end
