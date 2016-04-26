8779b0612963adfde69907567ae791a09ce93702
Josh Mills <jmills@covermymeds.com>
2016-04-26 15:04:57 -0400

    Created shared role tests for the Contact role.

--- test/unit/contact_test.rb
+++ test/unit/contact_test.rb
@@ -1,23 +1,11 @@
 require_relative '../test_helper'
+require_relative 'contact_role_shared'
 require 'contact'
 
 describe Contact do
-  let(:contact) { Contact.new(customer_name, customer_email, payment_type, selected_item) }
-  let(:customer_name)    { 'test' }
-  let(:customer_email)   { 'test@test.com' }
-  let(:payment_type)     { 'PayPal' }
-  let(:selected_item)    { 'Registration $100' }
+  include ContactRolePublicInterfaceTest
 
-  describe 'public interface' do
-    it 'behaves like a contact' do
-      assert contact.respond_to? :name
-      assert contact.respond_to? :email
-      assert contact.respond_to? :payment_type
-      assert contact.respond_to? :selected_item
-      assert contact.respond_to? :address
-      assert contact.respond_to? :payment_by_check?
-    end
-  end
+  let(:contact) { Contact.new(customer_name, customer_email, payment_type, selected_item) }
 
   describe '#address' do
     context 'with a name and email' do
